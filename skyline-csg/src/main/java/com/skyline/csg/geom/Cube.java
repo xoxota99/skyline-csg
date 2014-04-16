d9a8a75e28e9d6912933c27649afb966701f3c80
philippe.desrosiers <philippe.desrosiers@gmail.com>
2014-04-15 23:06:30 -0700

    More comments, cleaned up code for legibility.

--- skyline-csg/src/main/java/com/skyline/csg/geom/Cube.java
+++ skyline-csg/src/main/java/com/skyline/csg/geom/Cube.java
@@ -7,15 +7,16 @@ import javax.vecmath.*;
 import com.skyline.csg.*;
 
 public class Cube extends CSG {
-	private double radius;
-	private Vector3d center;
+	private double radius = 1;
+	private Vector3d center = new Vector3d(0, 0, 0);
 
 	public Cube() {
-		this(1f);
+		generatePolys();
 	}
 
 	public Cube(double radius) {
-		this(radius, new Vector3d(0, 0, 0));
+		this.radius = radius;
+		generatePolys();
 	}
 
 	/**
@@ -39,29 +40,40 @@ public class Cube extends CSG {
 				{ { 0, 2, 3, 1 }, { 0, 0, -1 }, { 2, 0, 1, 3 } },
 				{ { 4, 5, 7, 6 }, { 0, 0, +1 }, { 2, 0, 1, 3 } }
 		};
-		Vector3d c = (center == null ? new Vector3d(0, 0, 0) : center);
+
 		this.polygons = new ArrayList<Polygon>();
+		// for each side / poly
 		for (int[][] v2 : v) {
 			List<Vertex> vertices = new ArrayList<Vertex>();
+			// for each corner on a side.
 			for (int i : v2[0]) {
 				/*
 				 * v2[0] encodes 4 vertices. Each entry is a 3-bit integer that
 				 * encodes the unit extent for an axis. i & 1 > x extent. i & 2
 				 * > y i & 4 > z. Extent is either -1 or 1, then we multiply by
-				 * the radius and add the center.
+				 * the radius and add the center to get xyz coordinates. Neat
+				 * hack to encode xyz as a single integer. So if Radius is .5,
+				 * the cube will be 1x1x1.
 				 */
+				int xVal = (2 * ((i & 1) != 0 ? 1 : 0) - 1);
+				int yVal = (2 * ((i & 2) != 0 ? 1 : 0) - 1);
+				int zVal = (2 * ((i & 4) != 0 ? 1 : 0) - 1);
+				System.out.println("Cube vertex at (" + (center.x + this.radius * xVal)+", "+(center.x + this.radius * yVal)+", "+(center.x + this.radius * zVal)+")");
 				Vector3d pos = new Vector3d(
-						c.x + radius * (2 * ((i & 1) != 0 ? 1 : 0) - 1),
-						c.y + radius * (2 * ((i & 2) != 0 ? 1 : 0) - 1),
-						c.z + radius * (2 * ((i & 4) != 0 ? 1 : 0) - 1)
+						center.x + this.radius * xVal,
+						center.y + this.radius * yVal,
+						center.z + this.radius * zVal
 						);
 
+				// Normal for this vertex, which is the same as the normal for
+				// the side. All four vertices of a side share the same normal.
 				Vector3d norm = new Vector3d(v2[1][0], v2[1][1], v2[1][2]);
 
 				TexCoord2f tex = new TexCoord2f(
 						(v2[2][0] & 2) >> 1,
 						v2[2][0] & 1
 						);
+				// Not sure what v2[1..3] are used for...
 
 				Vertex vertex = new Vertex(pos, norm, tex);
 				vertices.add(vertex);
